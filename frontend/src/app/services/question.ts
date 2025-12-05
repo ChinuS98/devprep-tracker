0c8327e48e8cbcaf9700beb100ae1c250be5e454
Chinmay Sangelkar <chinmay.sangelkar@gmail.com>
2025-12-06 00:58:49 +0530

    Updated QuestionService to use environment API URL

--- frontend/src/app/services/question.ts
+++ frontend/src/app/services/question.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Question } from '../models/question.model';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environment/environment';
 
 @Injectable({
   providedIn: 'root'
